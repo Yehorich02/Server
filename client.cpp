b36ee8f80d2255ef73896d82225dc1ed62e9ed24
Yehorich02 <81953821+Yehorich02@users.noreply.github.com>
2021-04-19 18:15:29 +0300

    added CommandFunc

--- client.cpp
+++ client.cpp
@@ -2,7 +2,53 @@
 #include <iostream>
 #include <string>
 
-bool connected = false;
+class Client
+{
+private:
+    boost::asio::io_service service_;
+    boost::asio::ip::tcp::socket socket_;
+    boost::system::error_code error;
+    boost::asio::streambuf buf;
+
+
+public:
+    Client() : socket_(service_) {};
+
+    void ConnectToServer(std::string ip_address, uint16_t port)
+    {
+        socket_.connect(boost::asio::ip::tcp::endpoint(boost::asio::ip::address_v4::from_string(ip_address), port));
+        if (!error)
+        {
+            std::cout << "Connect to " << ip_address << ":" << port << std::endl;
+            std::cout << ReadFunc();
+        }
+        else
+        {
+            std::cerr << "An error oqurred: " << error.message() << std::endl;
+        }
+    }
+
+    std::string ReadFunc()
+    {
+        boost::asio::read_until(socket_, buf, "\n");
+        std::string data = boost::asio::buffer_cast<const char*>(buf.data());
+        return data;
+    }
+
+    void WriteFunc(const std::string& msg)
+    {
+        boost::asio::write(socket_, boost::asio::buffer(msg + "\n"));
+        if (!error)
+        {
+            std::cout << "Message sent to " << socket_.remote_endpoint().address() << ":"
+                << socket_.remote_endpoint().port() << ", with data: " << msg << std::endl;
+        }
+        else
+        {
+            std::cerr << "An error oqurred: " << error.message() << std::endl;
+        }
+    }
+};
 
 void HelpBox()
 {
@@ -17,53 +63,18 @@ void HelpBox()
         << std::endl;
 }
 
-//function for processing messages from the server
-std::string do_read(boost::asio::ip::tcp::socket& socket)
+void CommandFunc(Client& client)
 {
-    boost::asio::streambuf buf;
-    boost::asio::read_until(socket, buf, "\n");
-    std::string data = boost::asio::buffer_cast<const char*>(buf.data());
-    return data;
-}
-
-//function to send a message to the server
-void do_write(boost::asio::ip::tcp::socket& socket, const std::string& message)
-{
-    boost::asio::write(socket, boost::asio::buffer(message + "\n"));
-    std::cout << "Message sent to " << socket.remote_endpoint().address() << ":" << socket.remote_endpoint().port() << ", with data: " << message<< std::endl;
-}
-
-//function to connect to server
-void ConnectToServer(boost::asio::ip::tcp::socket& socket, std::string& ip_address, uint16_t& port, boost::system::error_code const& error)
-{
-    socket.connect(boost::asio::ip::tcp::endpoint(boost::asio::ip::address_v4::from_string(ip_address), port));
-    if (!error)
-    {
-        std::cout << "Connect to " << ip_address << ":" << port << std::endl;
-        std::cout << do_read(socket);
-        connected = true;
-    }
-    else
-    {
-        std::cerr << "An error oqurred: " << error.message() << std::endl;
-    }
-}
-
-
-
-int main(int argc, char* argv[])
-{
-    boost::asio::io_service io_service;
-    boost::asio::ip::tcp::socket socket_(io_service);
-    boost::system::error_code error;
     std::string command;
-    std::string ip; //client ip-address
-    uint16_t port; //the port number on which the server is running
     char value;
-    std::string name = "Client";
-    std::string str;
-    while (true) {
-        std::cout << name << "> ";
+    std::string ip;
+    uint16_t port;
+    std::string msg;
+    bool connected = false;
+
+    while (true)
+    {
+        std::cout << "Client> ";
         std::cin >> command;
         if (command == "help")
         {
@@ -80,25 +91,40 @@ int main(int argc, char* argv[])
             ip.pop_back();
             std::cin >> port;
 
-            ConnectToServer(socket_, ip, port, error);
+            client.ConnectToServer(ip, port);
+            connected = 1;
+
         }
         else if (command == "send" && connected)
         {
-            std::cin.ignore(1,' ');
-            std::getline(std::cin, str);
-            do_write(socket_, str);
-            
+            std::cin.ignore(1, ' ');
+            std::getline(std::cin, msg);
+            client.WriteFunc(msg);
+
         }
         else if (command == "exit")
         {
-            return 0;
+            return;
         }
         else
         {
-            std::cerr << "If you need help enter - help\r\n" << std::endl;
+            std::cerr << "If you need help enter - help" << std::endl;
+            std::getline(std::cin, command);
         }
-
         command.erase();
     }
+}
 
+int main(int argc, char* argv[])
+{
+    try
+    {
+        Client cl;
+        CommandFunc(cl);
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    return 0;
 }
